f9d0c3934a3eccd5622472c7276715c62972c37f
Gleb Moskalenko <glebmxyz@gmail.com>
2023-08-20 15:48:31 +0300

    fix toggle-hdmi.sh

--- .scripts/toggle-hdmi.sh
+++ .scripts/toggle-hdmi.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 intern=eDP1
-extern=HDMI-1-0
 
 if xrandr | grep -E "$extern (connected \(|disconnected)" &> /dev/null; then
-    xrandr --output "$intern" --auto --output "$extern" --auto
+    xrandr --output "$extern" --auto
 else
-    xrandr --output "$intern" --auto --output "$extern" --off
+    xrandr --output "$extern" --off
 fi
